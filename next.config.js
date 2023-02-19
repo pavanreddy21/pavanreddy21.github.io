e2c57ebcb001f483f343c43b78819a943a4f2f91
Pavan Reddy Gottimukkula <46338498+pavanreddy21@users.noreply.github.com>
2023-02-18 20:19:40 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   future: {
     webpack5: true,
+  },
+  images: {
+    loader: 'imgix',
+    path: '',
   },
   webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
